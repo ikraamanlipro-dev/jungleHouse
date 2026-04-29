a65658c8374d4000b61c6ec1ded5b4bf5b89ac71
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-04-29 06:36:43 +0300

    Composant de la barre de nav

---
+++ la-maison-jungle/src/components/Banner.jsx
@@ -0,0 +1,9 @@
+/**
+ * Banner est un composant pour présenter la bannière (barre de navigation)
+ */
+
+function Banner() { // crée une fonction !banner
+    return <h1> Bienvenue </h1> // Qui renvoie une balise  html <h1> bienvenue  
+}
+
+export default Banner
