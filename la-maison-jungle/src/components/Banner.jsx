03bac652d23183dae4470c6171177cc225ac2fe6
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-04-30 07:30:23 +0300

    Ajout et importation d'un logo

--- la-maison-jungle/src/components/Banner.jsx
+++ la-maison-jungle/src/components/Banner.jsx
@@ -1,9 +1,20 @@
 /**
  * Banner est un composant pour présenter la bannière (barre de navigation)
  */
+import styles from '../styles/Banner.module.css' // j'importe le fichier css de la bannière
+import logo from '../assets/logo.jpg' // j'importe le logo de la bannière
 
-function Banner() { // crée une fonction !banner
-    return <h1> Bienvenue </h1> // Qui renvoie une balise  html <h1> bienvenue  
+const Banner = () => { // crée une fonction Banner
+    const title = 'Ben-venido à la casa' // je crée une variable title qui contient le titre de la bannière
+    
+
+    return(
+        <div className={styles.banner}>
+            <img src={logo} className={styles.logo} alt="Logo" />
+            <h1 className={styles.title}>{title}</h1> 
+            
+        </div>
+    )
 }
 
 export default Banner
