5cbdf0d2a7ff1695f7b7ee3b6371d7b09d181ce5
ikraamanlipro-dev <ikraamanli.pro@gmail.com>
2026-04-30 07:27:03 +0300

    Importation du composant cart

--- la-maison-jungle/src/App.jsx
+++ la-maison-jungle/src/App.jsx
@@ -5,15 +5,20 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 // J'import le composant Banner.jsx
-import Banner from './components/Banner'
+import Banner from './components/Banner' // j'importe le composant Banner depuis le dossier components
 
-function App() {
+// J'import le composant Cart.jsx
+import Cart from './components/Cart' // j'importe le composant Cart depuis le dossier components
 
-  return (
+function App() { // je crée un composant App sous forme de fonction . Le composant renvoie le composant Banner
+
+  return ( 
     <>
-      <Banner />
+      <Banner /> {/* J'appelle le composant Banner dans App sous d'une balise */}
+
+      <Cart /> {/* J'appelle le composant Cart dans App sous d'une balise */}
     </>
   )
 }
 
-export default App
+export default App // j'exporte le composant App 
